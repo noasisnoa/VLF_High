97bb02e6f6647cdc2e46e8202b44fb6205bb8bc0
noasisnoa <106498526+noasisnoa@users.noreply.github.com>
2022-12-08 18:09:42 +0900

    Update sketch_dec04g.ino

--- Arduino 3. Motor control using ultrasonic sensors/sketch_dec04g.ino
+++ Arduino 3. Motor control using ultrasonic sensors/sketch_dec04g.ino
@@ -124,8 +124,6 @@ void setup() {
   pinMode(Left_Pin2, OUTPUT);
   pinMode(Right_Pin1, OUTPUT);
   pinMode(Right_Pin2, OUTPUT);
-  analogWrite(Left, 255);
-  analogWrite(Right, 255);
 
   pinMode(F_Sonar, OUTPUT);
   pinMode(L_Sonar, OUTPUT);
